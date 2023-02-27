5ace9b04f22085f03e942679b9e5dd94d5fd81be
Damian <kakaka123321@wp.pl>
2023-02-27 19:57:55 +0100

    Modify product table and add filament config

--- database/migrations/2023_02_27_171211_create_products_table.php
+++ database/migrations/2023_02_27_171211_create_products_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->decimal('price', 9, 3);
+            $table->string('description');
             $table->timestamps();
         });
     }
